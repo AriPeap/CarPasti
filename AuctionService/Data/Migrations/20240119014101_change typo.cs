fc52a1e5bb89f1e7b94648e8b6b7799290bae7e2
Clancy Wang <pasti@Clancys-MacBook-Pro.local>
2024-01-20 22:59:35 +0800

    build basic curd

---
+++ AuctionService/Data/Migrations/20240119014101_change typo.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class changetypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Actions_AuctionId",
+                table: "Items");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Actions",
+                table: "Actions");
+
+            migrationBuilder.RenameTable(
+                name: "Actions",
+                newName: "Auctions");
+
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Items",
+                newName: "ImageUrl");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Auctions",
+                table: "Auctions",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Auctions_AuctionId",
+                table: "Items",
+                column: "AuctionId",
+                principalTable: "Auctions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Auctions_AuctionId",
+                table: "Items");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Auctions",
+                table: "Auctions");
+
+            migrationBuilder.RenameTable(
+                name: "Auctions",
+                newName: "Actions");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Items",
+                newName: "MyProperty");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Actions",
+                table: "Actions",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Actions_AuctionId",
+                table: "Items",
+                column: "AuctionId",
+                principalTable: "Actions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
